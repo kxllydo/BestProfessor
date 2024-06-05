d2715e71c543951a6ed10eba0cdb1102303292d8
Kelly Do <kellydo8467@gmail.com>
2024-06-05 12:36:36 -0400

    new project

---
+++ bestProfessor/src/main/java/bestProfessor/bestProfessor/BestProfessorApplication.java
@@ -0,0 +1,13 @@
+package bestProfessor.bestProfessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestProfessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestProfessorApplication.class, args);
+	}
+
+}
---
+++ bestProfessor/src/test/java/bestProfessor/bestProfessor/BestProfessorApplicationTests.java
@@ -0,0 +1,13 @@
+package bestProfessor.bestProfessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestProfessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
