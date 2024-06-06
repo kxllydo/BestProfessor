d2fbfbf59af090e5fb16dcdf5b492c6f13bc7a88
Kelly Do <kellydo8467@gmail.com>
2024-06-06 12:57:00 -0400

    installed vue and hid node_modules and target folder

--- src/main/java/bestProfessor/bestProfessor/BestProfessorApplication.java
+++ src/main/java/bestProfessor/bestProfessor/BestProfessorApplication.java
@@ -8,6 +8,6 @@ public class BestProfessorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BestProfessorApplication.class, args);
+		System.out.println("hello");
 	}
-
 }
