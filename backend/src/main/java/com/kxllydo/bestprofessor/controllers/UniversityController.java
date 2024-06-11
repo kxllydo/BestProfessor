05e69430b4d6b4c1510cd4f98aded8c129fffe57
steviels <118238179+steviels@users.noreply.github.com>
2024-06-11 10:07:45 -0400

    jsoup to scrape university name results

---
+++ backend/src/main/java/com/kxllydo/bestprofessor/controllers/UniversityController.java
@@ -0,0 +1,31 @@
+package com.kxllydo.bestprofessor.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
+@RestController
+public class UniversityController {
+
+    @GetMapping("/api/match-university/{university-name}")
+    public String matchUniversity(@PathVariable(name = "university-name", required = true) String universityName) {
+        Document doc;
+https://www.ratemyprofessors.com/search/schools?q=asdfae
+        try {
+            System.out.println("----------------------------------------------------------------------------------------------------------------");
+            doc = Jsoup.connect("https://www.ratemyprofessors.com/search/schools?q=" + universityName.replaceAll(" ", "%20")).get();
+            Elements el = doc.select("a.SchoolCard__StyledSchoolCard-sc-130cnkk-0 div.SchoolCardHeader__StyledSchoolCardHeader-sc-1gq3qdv-0");
+            System.out.println(el);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return universityName;
+    }
+}
