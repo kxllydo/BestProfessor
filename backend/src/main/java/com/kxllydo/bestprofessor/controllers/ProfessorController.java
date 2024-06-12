4cec7f26007ea77fb1c2dfff4e781745451362ab
Kelly Do <kellydo8467@gmail.com>
2024-06-12 09:31:11 -0400

    finished the professor options endpoint

--- backend/src/main/java/com/kxllydo/bestprofessor/controllers/ProfessorController.java
+++ backend/src/main/java/com/kxllydo/bestprofessor/controllers/ProfessorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.time.Duration; 
 
 import org.jsoup.*;
 import org.jsoup.nodes.*;
@@ -28,40 +29,27 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class ProfessorController {
 
     @GetMapping("/api/professor-options/{professor-name}")
-    public Query professorOptions(@PathVariable(name = "professor-name", required = true) String professorName, WebDriver driver, WebDriverWait wait) {
-        Document doc;
-        List<String> response = new ArrayList<>();
-        String query = "https://www.ratemyprofessors.com/search/professors/1521?q=" + professorName.replaceAll(" ", "%20");
-        
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("CardName__StyledCardName-sc-1gyrgim-0.cJdVEK")));
-        List <WebElement> erm = driver.findElements(By.className("CardName__StyledCardName-sc-1gyrgim-0.cJdVEK"));
-        for (WebElement x : erm){
-            System.out.println(x.getText());
-        }
-
-        // try {
-        //     doc = Jsoup.connect(query).get();
-        //     Elements names = doc.select("div.CardName__StyledCardName-sc-1gyrgim-0.cJdVEK");
-        //     System.out.println(names);
-
-        //     for (Element name: names){
-        //         String prof = name.text();
-        //         System.out.println(prof);
-        //         response.add(prof);
-        //     }
-        // } catch (IOException e) {
-        //     throw new RuntimeException(e);
-        // } 
-        // return new Query(query, response);
-    }
-
-    public static void main(String[] args) {
+    public Query professorOptions(@PathVariable(name = "professor-name", required = true) String professorName) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\do-kelly\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        professorOptions("mark boady", driver, wait);
+        List<String> response = new ArrayList<>();
+        String query = "https://www.ratemyprofessors.com/search/professors/1521?q=" + professorName.replaceAll(" ", "%20");
 
+        try {
+            driver.get(query);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".CardName__StyledCardName-sc-1gyrgim-0.cJdVEK")));
+            List<WebElement> elements = driver.findElements(By.cssSelector(".CardName__StyledCardName-sc-1gyrgim-0.cJdVEK"));
+            for (WebElement element : elements) {
+                String professor = element.getText();
+                response.add(professor);
+            }
+        } finally {
+            driver.quit(); // Ensure the WebDriver is properly closed
+        }
 
+        return new Query(query, response);
     }
+    
 }
