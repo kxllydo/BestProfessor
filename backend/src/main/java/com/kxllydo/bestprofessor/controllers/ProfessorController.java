60c8ce80a29a28a299cd12564464440f0ef76ba9
steviels <118238179+steviels@users.noreply.github.com>
2024-06-16 01:46:26 -0400

    finish get all profs(test ver)

--- backend/src/main/java/com/kxllydo/bestprofessor/controllers/ProfessorController.java
+++ backend/src/main/java/com/kxllydo/bestprofessor/controllers/ProfessorController.java
@@ -2,7 +2,6 @@ package com.kxllydo.bestprofessor.controllers;
 import com.kxllydo.bestprofessor.models.Professor;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.JavaScriptUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.TimeoutException;
@@ -26,40 +26,77 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 @RestController
 public class ProfessorController {
 
-    public void clickDepartmentFilter(WebDriver driver, String department) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        JavascriptExecutor driverJs = (JavascriptExecutor) driver;  
+    /**
+     * Attempts to click the element, and will remove any obscuring elements in the way.
+     * Assumption that element is ready to be clicked.
+     * 
+     * @param driver
+     * @param element
+     */
+    public void attemptToClick(WebDriver driver, WebElement element) {
+        JavascriptExecutor driverJs = (JavascriptExecutor) driver;
         Actions action = new Actions(driver);
-        
-        try {
-            WebElement popup = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".ReactModal__Overlay.ReactModal__Overlay--after-open.FullPageModal__StyledFullPageModal-sc-1tziext-1.fyxlwo__overlay")));
 
-            driverJs.executeScript("arguments[0].remove();", popup);
-        } catch (TimeoutException e) {
-            System.out.println("Popup not found.");
+        try {
+            action.scrollToElement(element)
+                  .moveToElement(element)
+                  .perform();
+            element.click();
+        } catch (ElementClickInterceptedException e) {
+            try { Thread.sleep(1000); } catch (Exception e2) {}
+
+            List<WebElement> hoverElements = (List<WebElement>) driverJs.executeScript("return document.querySelectorAll(':hover');");
+            WebElement hoverElement;
+
+            if (hoverElements.size() > 0)
+                hoverElement = hoverElements.get(hoverElements.size() - 1);
+            else
+                throw new RuntimeException(e);
+
+            driverJs.executeScript("arguments[0].remove();", hoverElement);
+            attemptToClick(driver, element);
         } catch (Exception e) {
+            driver.quit();
             throw new RuntimeException(e);
         }
+    }
 
+
+    /**
+     * Opens the department filter dropdown and selects the given department, if present.
+     * 
+     * @param driver
+     * @param department
+     */
+    public void clickDepartmentFilter(WebDriver driver, String department) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
+    
+        // Clicks on dropdown menu
         WebElement dropdown = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".css-1l6bn5c-control")));
-        action.scrollToElement(dropdown).click(dropdown).perform();
+        attemptToClick(driver, dropdown);
 
+        // Gathers all department names
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".css-1u8e7rt-menu")));
         List<WebElement> departmentElements = driver.findElements(By.cssSelector(".css-l0mlil-option"));
         List<String> departmentNames = departmentElements.stream().map(element -> element.getText().toLowerCase()).toList();
         
+        // Searches for department argument in list of department names & clicks
         int index = departmentNames.indexOf(department);
-        if (index < 0)
+        if (index < 0) {
+            System.out.println("^".repeat(99));
+            System.out.println(department);
+            System.out.println("v");
+            for (String s: departmentNames) {System.out.println(s);}
             throw new RuntimeException("Could not find department.");
-        else {
+        } else {
             WebElement departmentElement = departmentElements.get(index);
-            action.scrollToElement(departmentElement).click(departmentElement).perform();
-            // departmentElement.click();
+            attemptToClick(driver, departmentElement);
 
+            // Waits for reload?
             try {
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".css-1l6bn5c-control")));
-            } catch (TimeoutException e) {
             } catch (Exception e) {
+                driver.quit();
                 throw new RuntimeException(e);
             }
         }
@@ -74,7 +111,6 @@ public class ProfessorController {
     public void clickShowMore(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         JavascriptExecutor driverJs = (JavascriptExecutor) driver;
-        int i = 0;
 
         WebElement button;
 
@@ -89,37 +125,32 @@ public class ProfessorController {
         Pattern buttonText = Pattern.compile("Show More");
 
         try {
-            while (i < 2 && wait.until(ExpectedConditions.textMatches(By.cssSelector("button.Buttons__Button-sc-19xdot-1"), buttonText))) {
-                i += 1;
+            while (wait.until(ExpectedConditions.textMatches(By.cssSelector("button.Buttons__Button-sc-19xdot-1"), buttonText))) {
                 driverJs.executeScript("arguments[0].click();", button);
             }
         } catch (TimeoutException e) {
         } catch (Exception e) {
+            driver.quit();
             throw new RuntimeException(e);
         }
     }
+
 
     @GetMapping("/api/professors")
     public ArrayList<Professor> getProfessors(@RequestParam(name = "school", required = true) int schoolId, @RequestParam(name = "department", required = false) String department) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.get(String.format("https://www.ratemyprofessors.com/search/professors/%d?q=*", schoolId));
 
         try {
             if (department != null)
                 clickDepartmentFilter(driver, department);
-
-            // long startTime = System.nanoTime() / (long) 1e9;
             clickShowMore(driver);
-            // long endTime = System.nanoTime() / (long) 1e9;
-            // System.out.println(endTime - startTime);
         } catch (Exception e) {
-            System.out.println(e);
             driver.quit();
             throw new RuntimeException(e);
-            // return null;
         }
 
         ArrayList<Professor> professors = new ArrayList<>();
@@ -142,37 +173,4 @@ public class ProfessorController {
         driver.quit();
         return professors;
     }
-
-    @GetMapping("/api/test")
-    public String testing() {
-        WebDriver driver = new ChromeDriver();
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        driver.get("www.google.com");
-        js.executeScript("let a = arguments[0].getElementsByTagName('iframe'); for (let i = 0; i < a.length; i++) {a[i].remove();}", driver.findElement(By.id("root")));
-        return "asd";
-    }
-
-    // @GetMapping("/api/professor-options/{professor-name}")
-    // public Query professorOptions(@PathVariable(name = "professor-name", required = true) String professorName) {
-    //     System.setProperty("webdriver.chrome.driver", "C:\\Users\\do-kelly\\Downloads\\chromedriver.exe");
-    //     WebDriver driver = new ChromeDriver();
-    //     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-
-    //     List<String> response = new ArrayList<>();
-    //     String query = "https://www.ratemyprofessors.com/search/professors/1521?q=" + professorName.replaceAll(" ", "%20");
-
-    //     try {
-    //         driver.get(query);
-    //         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".CardName__StyledCardName-sc-1gyrgim-0.cJdVEK")));
-    //         List<WebElement> elements = driver.findElements(By.cssSelector(".CardName__StyledCardName-sc-1gyrgim-0.cJdVEK"));
-    //         for (WebElement element : elements) {
-    //             String professor = element.getText();
-    //             response.add(professor);
-    //         }
-    //     } finally {
-    //         driver.quit(); // Ensure the WebDriver is properly closed
-    //     }
-
-    //     return new Query(query, response);
-    // }
 }
